e0b08a2f144b0b3f0867a607947ff6755e460086
Diego Caucota <diegocaucota@gmail.com>
2025-08-07 10:39:16 -0300

    add: main documentation

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Importar el componente App y renderizarlo en el elemento con id 'root'
+// Utilizar StrictMode para resaltar problemas potenciales en la aplicación
+// createRoot es una función de React 18 para crear un root de la aplicación
+// renderizar el componente App dentro del root creado
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
