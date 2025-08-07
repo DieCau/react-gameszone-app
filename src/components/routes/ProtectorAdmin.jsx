e0fea812840fedc85d5c9c568fdb2afa096ec30e
Diego Caucota <diegocaucota@gmail.com>
2025-08-07 10:37:31 -0300

    add: components routes ProtectorAdmin documentation

--- src/components/routes/ProtectorAdmin.jsx
+++ src/components/routes/ProtectorAdmin.jsx
@@ -1,5 +1,8 @@
 import { Navigate, Outlet } from "react-router";
 
+// Componente ProtectorAdmin que verifica si el usuario es administrador
+// Si no es administrador, redirige a la página principal
+// Recibe la propiedad isAdmin que indica si el usuario es administrador
 const ProtectorAdmin = ({ isAdmin }) => {
   if (!isAdmin) {
     return <Navigate to={"/"}></Navigate>;
